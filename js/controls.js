ffaa0f33652cc2152203e4fa41ad8be736abf41e
salimsyc <syusuf639@gmail.com>
2024-06-22 13:34:51 +0300

    Pre versions 1.0

--- js/controls.js
+++ js/controls.js
@@ -1,14 +1,57 @@
+let currentSlide = 0;
 
-let slideIndex = 0;
+const previousButton = document.querySelector(".previous-slide");
+const nextButton = document.querySelector(".next-slide");
 
-function moveCarousel(n) {
-    const slides = document.querySelectorAll('.text-carousel');
-    slideIndex += n;
-    if (slideIndex >= slides.length) {
-        slideIndex = 0;
+const slides = document.querySelectorAll('.carousel-content');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    if (index >= totalSlides) {
+        currentSlide = 0;
+    } else if (index < 0) {
+        currentSlide = totalSlides - 1;
+    } else {
+        currentSlide = index;
     }
-    if (slideIndex < 0) {
-        slideIndex = slides.length - 1;
-    }
-    slides.forEach(slide => slide.style.transform = 'translateX(-${slideIndex * 100}%)');
+
+    const newTransform = -currentSlide * 100 + "%";
+    document.querySelector(".carousel-content").style.transform = `translateX(${newTransform})`;
+
+    slides.forEach((slide, i) => {
+        slide.classList.toggle("active", i === currentSlide);
+    });
 }
+
+// Initialize the first slide
+setInterval(nextSlide, 3000);
+
+function previousSlide() {
+    showSlide(currentSlide - 1);
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+previousButton.addEventListener("click", previousSlide);
+nextButton.addEventListener("click", nextSlide);
+
+
+const customerTestimonialsRightbtnScroll = document.querySelector(".testimonials-rightbtn-scroll");
+const customerTestimonialsLeftbtnScroll = document.querySelector(".testimonials-leftbtn-scroll");
+const customersTestimonialsContainer = document.querySelector(".customers-container");
+
+customersTestimonialsContainer.addEventListener("wheel", (event) => {
+    event.preventDefault();
+    customersTestimonialsContainer.scrollLeft += event.deltaY;
+});
+
+customerTestimonialsLeftbtnScroll.addEventListener("click", ()=> {
+    customersTestimonialsContainer.scrollLeft += 400;
+});
+
+customerTestimonialsRightbtnScroll.addEventListener("click", () => {
+    customersTestimonialsContainer.scrollLeft -= 400;
+});
+
